ac7b73d55e5a80c2fb7263b3fa8c81e397218021
Etsegenet <114258837+alamerewetsegenet@users.noreply.github.com>
2024-03-12 19:57:32 +0000

    this push it!

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
   path("",views.home, name="home"),
+  path("about/",views.about, name="about"),
+  path('function/', views.function_view, name='function_view'),
+  path('class/', views.ClassView.as_view(), name='class_view'),
+  path('theme/', views.ThemeView.as_view(), name='theme'),
+  path("surf/",views.surf, name="surf"),
 
-  path("about",views.home, name="about"),
 ]
+
