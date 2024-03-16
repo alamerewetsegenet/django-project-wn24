087828974118785da2ce58d5fd8dc808e4fd1f47
Etsegenet <114258837+alamerewetsegenet@users.noreply.github.com>
2024-03-16 21:17:41 +0000

    Invention

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from . import views
+from .views import InventionListView, InventionDetailView
 
 urlpatterns = [
-  path("",views.home, name="home"),
-  path("about/",views.about, name="about"),
+  path("", views.home_view, name="home"),
+  path("about/",views.about_view, name="about"),
   path('function/', views.function_view, name='function_view'),
   path('class/', views.ClassView.as_view(), name='class_view'),
   path('theme/', views.ThemeView.as_view(), name='theme'),
-  path("surf/",views.surf, name="surf"),
+  path('load/', views.load_default_data_view, name='load_default_data'),
+  path("surf/",views.surf_view, name="surf"),
+  
+  path('inventions/', views.InventionListView.as_view(), name='invention-list'),
+  path('invention/<int:pk>/', views.InventionDetailView.as_view(), name='invention-view'),
+
 
 ]
 
